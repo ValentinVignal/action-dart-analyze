4ca82f8083eb4780fbf976eae2a4ed3b7d0ab236
Dan Gilbert <ddg@daentech.co.uk>
2021-11-18 13:03:29 +0100

    Make analysis_options respect the working directory

--- src/utils/IgnoredFiles.ts
+++ src/utils/IgnoredFiles.ts
@@ -2,19 +2,21 @@ import * as yaml from 'js-yaml';
 import * as fs from 'fs';
 // import { minimatch } from 'minimatch';
 import * as minimatch from 'minimatch';
+import * as path from 'path';
+import { actionOptions } from './ActionOptions';
 
 /**
  * The ignore files in the analysis_options.yaml
  */
-export class IgnoredFiles{
+export class IgnoredFiles {
   private readonly patterns: minimatch.IMinimatch[];
   constructor() {
     let patterns: string[];
     try {
-      const yamlFile = yaml.load(fs.readFileSync('./analysis_options.yaml', 'utf8')) as {analyzer?: {exclude?: string[]}};
+      const yamlFile = yaml.load(fs.readFileSync(path.resolve(actionOptions.workingDirectory, './analysis_options.yaml'), 'utf8')) as { analyzer?: { exclude?: string[] } };
       patterns = yamlFile?.analyzer?.exclude ?? [];
     } catch (error) {
-      console.log(`Could not load analysis_options.yml:\n${error}`);
+      console.log(`Could not load analysis_options.yaml:\n${error}`);
     }
     patterns ??= [];
     this.patterns = patterns.map((pattern) => new minimatch.Minimatch(pattern));
