65546aa1475eb22ad6d40d52a28eac6991402b71
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-08-01 14:25:11 +0800

    :mute: Remove logs

--- src/format/Format.ts
+++ src/format/Format.ts
@@ -41,18 +41,12 @@ export async function format(params: { modifiedFiles: ModifiedFiles, ignoredFile
   const errLines = errOutputs.trim().split(/\r?\n/);
   const fileNotFormatted = new Set<string>();
 
-  for (const m of params.modifiedFiles.files) {
-    console.log('modifiedFile:', m[0], '- name:', m[1].name);
-  }
-
   for (const line of [...lines, ...errLines]) {
     if (!line.startsWith('Changed')) {
       continue;
     }
 
     const file = line.split(' ')[1];
-    console.log('file:', file);
-    console.log('file with join:', path.join(actionOptions.workingDirectory, file));
     // There is not need to use the `currentWorkingDirectory` here because the
     // `ignoredFiles` a minimatch from the working directory.
     if (params.ignoredFiles.has(file)) {
