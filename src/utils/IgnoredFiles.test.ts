b1f0d5f4f394b3bea904f8f5834ad97897e1e1b4
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-08-14 16:46:36 +0800

    tests: Add test for analysis_options that is not directly in the working directory

--- src/utils/IgnoredFiles.test.ts
+++ src/utils/IgnoredFiles.test.ts
@@ -16,7 +16,9 @@ jest.mock('path', () => mockPath);
 
 const mockFs = {
   readFileSync: jest.fn(),
+  existsSync: jest.fn(),
 };
+
 
 jest.mock('fs', () => mockFs);
 
@@ -44,6 +46,7 @@ describe('IgnoredFiles', () => {
     } as ActionOptions;
 
     mockPath.resolve.mockReturnValue('path/to/yaml');
+    mockFs.existsSync.mockReturnValue(true);
     mockFs.readFileSync.mockReturnValue('yamlContent');
     mockYaml.load.mockReturnValue({
       analyzer: {
@@ -60,6 +63,48 @@ describe('IgnoredFiles', () => {
     ]);
     expect(mockFs.readFileSync.mock.calls).toEqual([
       ['path/to/yaml', 'utf8'],
+    ]);
+    expect(mockYaml.load.mock.calls).toEqual([
+      ['yamlContent'],
+    ]);
+
+    expect(ignoredFiles.has('lib/path/to/generated/file.g.dart')).toBe(true);
+    expect(ignoredFiles.has('lib/path/to/normal/file')).toBe(false);
+    expect(ignoredFiles.has('lib/main.dart')).toBe(false);
+    expect(ignoredFiles.has('lib/excluded.dart')).toBe(true);
+  });
+  test('It should return the ignored files of the closest analysis_options.yaml file', () => {
+    mockActionOptions.actionOptions = {
+      workingDirectory: 'working/directory',
+    } as ActionOptions;
+
+    mockPath.resolve.mockReturnValueOnce('working/directory/analysis_options.yaml');
+    mockFs.existsSync.mockReturnValueOnce(false); // The file is not found.
+    mockPath.resolve.mockReturnValueOnce('working'); // Returns the parent
+    mockPath.resolve.mockReturnValueOnce('working/analysis_options.yaml'); // Yaml higher in the file tree.
+    mockFs.existsSync.mockReturnValueOnce(true); // The file is not found.
+    mockFs.readFileSync.mockReturnValue('yamlContent');
+    mockYaml.load.mockReturnValue({
+      analyzer: {
+        exclude: [
+          '**/*.g.dart',
+          'lib/excluded.dart'
+        ],
+      }
+    });
+    const ignoredFiles = new IgnoredFiles();
+
+    expect(mockPath.resolve.mock.calls).toEqual([
+      ['working/directory', 'analysis_options.yaml'],
+      ['working/directory', '..'],
+      ['working', 'analysis_options.yaml'],
+    ]);
+    expect(mockFs.existsSync.mock.calls).toEqual([
+      ['working/directory/analysis_options.yaml'],
+      ['working/analysis_options.yaml'],
+    ]);
+    expect(mockFs.readFileSync.mock.calls).toEqual([
+      ['working/analysis_options.yaml', 'utf8'],
     ]);
     expect(mockYaml.load.mock.calls).toEqual([
       ['yamlContent'],
