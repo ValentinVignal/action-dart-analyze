8dc5c1842655d4ac592adf35d06706c83d2b5fd9
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-01-29 23:34:51 +0800

    use script branch

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/ValentinVignal/action-dart-analyze/refs/heads/main/dist/index.js -o index.js
+curl https://raw.githubusercontent.com/ValentinVignal/action-dart-analyze/refs/heads/script/dist/index.js -o index.js
 node index.js
