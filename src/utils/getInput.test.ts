dbc574a4105e9a89d38eb94cbc678a195a429457
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-01-30 11:53:22 +0800

    :white_check_mark: Add tests

---
+++ src/utils/getInput.test.ts
@@ -0,0 +1,52 @@
+import { getInputSafe } from './getInput';
+
+describe('getInput', () => {
+  let processEnv: NodeJS.ProcessEnv;
+
+  beforeEach(() => {
+    processEnv = process.env;
+  });
+
+  afterEach(() => {
+    process.env = processEnv;
+  });
+
+  describe('with "-" in the name', () => {
+    it('should return empty string if there is no value', () => {
+      const value = getInputSafe('fail-on');
+      expect(value).toBe('');
+    });
+
+
+    it('should return the value set by the bash script', () => {
+      process.env.INPUT_FAIL_ON = 'warning';
+      const value = getInputSafe('fail-on');
+      expect(value).toBe('warning');
+    });
+
+    it('should return the value set by github action', () => {
+      process.env['INPUT_FAIL-ON'] = 'warning';
+      const value = getInputSafe('fail-on');
+      expect(value).toBe('warning');
+    });
+
+  });
+
+  describe('without "-" in the name', () => {
+    it('should return empty string if there is no value', () => {
+      const value = getInputSafe('emojis');
+      expect(value).toBe('');
+    });
+
+    it('should throw an error if there is no value and it is required', () => {
+      const f = () => getInputSafe('token', { required: true });
+      expect(f).toThrowError();
+    });
+
+    it('should throw an error if there is no value and it is required', () => {
+      process.env.INPUT_TOKEN = 'token';
+      const value = getInputSafe('token', { required: true });
+      expect(value).toBe('token');
+    });
+  });
+});
